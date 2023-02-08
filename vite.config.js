fd44a7df4e66f51e5fa2749fc4b691ff256f9414
NaicoQ <diego.diazdu@gmail.com>
2023-02-08 13:14:39 -0300

    #fix 07 | Try to fix React #130 Error

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    process.env.MODE !== "production"
+      ? react({
+          jsxRuntime: "classic",
+        })
+      : react(),
+  ],
   mode: "development",
   build: {
     minify: false,
   },
+  /*    react()],
+  mode: "development",
+  build: {
+    minify: false,
+  } */
 });
